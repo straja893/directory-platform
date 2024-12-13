d8dfa250e6dbba3d2dcbeda716c10a84afbec2f5
strajaDev <straja.dev@hotmail.com>
2024-12-13 17:16:09 +0100

    create navbar add auth logic

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { auth, signOut, signIn } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="px-3 py-5 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="">
+          <Image src="/logo.webp" alt="logo" width={50} height={50} />
+        </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.user.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Log In</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
